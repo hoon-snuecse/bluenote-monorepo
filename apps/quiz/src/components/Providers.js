991926edf578d96857134e4d092887b5a7bd1c31
Jong Hoon Kim <hoon@snuecse.org>
2025-07-30 21:24:10 +0900

    fix: Quiz 앱 React Hooks 오류 해결
    - SessionProvider 복원 (useSession 훅 사용을 위해 필요)
    - 메인 사이트와 동일한 세션을 사용하되, Provider는 필요
    - React error #310 해결

--- apps/quiz/src/components/Providers.js
+++ apps/quiz/src/components/Providers.js
@@ -1,7 +1,12 @@
 'use client'
 
+import { SessionProvider } from 'next-auth/react'
+
 export function Providers({ children }) {
-  // Quiz 앱은 메인 사이트의 세션을 사용하므로
-  // 자체 SessionProvider를 사용하지 않음
-  return <>{children}</>
+  // Quiz 앱도 SessionProvider가 필요함 (메인 사이트와 동일한 세션 사용)
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  )
 }
